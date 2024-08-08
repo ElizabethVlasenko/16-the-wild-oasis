e6e05fc6934b2b2b68d130281ec60fc102303683
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-08-08 12:12:21 +0100

    Added starter files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,22 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "react-app",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "react", "react-hooks"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "no-unused-vars": [2, { vars: "all", args: "after-used" }],
+    "no-undef": "warn",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
