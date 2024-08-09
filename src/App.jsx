f4fedf0e3d4dfb92e64b7bddcca87e77230c93f8
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-08-09 16:55:51 +0100

    Created Button, Header, Input and Row styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello Vite</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row type="horizontal">
+          <Heading as="h1">Hello Vite</Heading>
+          <div>
+            <Heading as="h2">Check in and out</Heading>
+            <Button>Check in</Button>
+            <Button variation="secondary" size="small">
+              Check out
+            </Button>
+          </div>
+        </Row>
+        <Row>
+          <Heading as="h3">Form</Heading>
+          <form>
+            <Input type="text" />
+            <Input type="number" />
+          </form>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
