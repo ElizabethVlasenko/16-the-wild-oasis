f46a44fe0eef85bbd4042a6b75565ebe73627336
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-08-22 15:22:21 +0100

    Added an account page where the user can change their name, avatar and password

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -47,4 +47,53 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  //update password OR fullName
+  let updateData;
+
+  if (password) updateData = { password };
+
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error: updateError } = await supabase.auth.updateUser(
+    updateData
+  );
+
+  if (updateError) {
+    console.error(updateError);
+    throw new Error(updateError.message);
+  }
+
+  if (!avatar) return data;
+
+  //upload avatar image
+
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: avatarUploadError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (avatarUploadError) {
+    console.error(avatarUploadError);
+    throw new Error(avatarUploadError.message);
+  }
+
+  //update avatar in the user
+
+  const { data: updatedUser, error: userAvatarError } =
+    supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+
+  if (userAvatarError) {
+    console.error(userAvatarError);
+    throw new Error(userAvatarError.message);
+  }
+
+  return updatedUser;
 }
