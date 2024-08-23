7ce7b02797e36157e9098a3e0cfe725be6eadd33
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-08-23 17:40:50 +0100

    Added a chart of stay duration for the dashboard

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -59,7 +59,10 @@ function SalesChart({ bookings, numDays }) {
 
   return (
     <StyledSalesChart>
-      <Heading as="h2">Sales</Heading>
+      <Heading as="h2">
+        Sales from {format(allDates.at(0), "do MMMM yyyy")} &mdash;{" "}
+        {format(allDates.at(-1), "do MMMM yyyy")}
+      </Heading>
       <ResponsiveContainer height={300} width="100%">
         <AreaChart data={data}>
           <XAxis
