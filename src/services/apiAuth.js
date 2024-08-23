5f1230ab64a45a5883aa8b86cc9978215bc85dc8
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-08-23 12:22:46 +0100

    Added a dark mode

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -84,7 +84,7 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
   //update avatar in the user
 
   const { data: updatedUser, error: userAvatarError } =
-    supabase.auth.updateUser({
+    await supabase.auth.updateUser({
       data: {
         avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
       },
